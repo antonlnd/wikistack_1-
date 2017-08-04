765e8b8f315c966a6a45513a6b94b3de0b3885a2
Jason Rosso <jasonrosso@gmail.com>
2017-08-04 17:17:58 -0400

    v2

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var nunjucks = require('nunjucks');
 var index = require('./routes/index');
 var users = require('./routes/users');
 
@@ -12,7 +12,10 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+var env = nunjucks.configure('views', {noCache: true});
+app.set('view engine', 'nunjucks');
+app.engine('html', nunjucks.render);
+
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -40,7 +43,6 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
 });
 
 module.exports = app;
