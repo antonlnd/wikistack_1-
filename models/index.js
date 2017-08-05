923e9de7b1d7763f317b3eb5fab9d56945708432
Land4 <land4@land.local>
2017-08-05 03:11:29 -0400

    update seed

--- models/index.js
+++ models/index.js
@@ -1,5 +1,72 @@
 var Sequelize = require('sequelize');
-var db = new Sequelize('postgres://localhost:5432/wikistack_1');
+var db = new Sequelize('postgres://localhost:4000/wikistack_1');
+
+var page = db.define('page',  {
+	title: {
+		type: Sequelize.STRING
+	},
+	urlTitle: 	{
+		type: Sequelize.STRING
+	},
+	content: {
+		type: Sequelize.STRING
+	},
+	status: {
+		type: Sequelize.STRING
+	}
+});
+
+var User = db.define('user',  {
+	name: {
+		type: Sequelize.STRING
+	},
+	email: 	{
+		type: Sequelize.STRING
+	}
+	});
+
+db.sync().then(ret  => console.log('good job!'));
+
+module.exports = {
+Page: Page,
+User: User
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 module.exports = sequelize;
